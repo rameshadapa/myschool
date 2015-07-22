03049b8ba7fdeb8e1826379033119fc6cb8b725d
Ramesh Adapa <ramesh.adapa86@gmail.com>
2015-07-22 22:54:30 +0530

    Added first Web Sphere.

---
+++ ws/installschool.php
@@ -0,0 +1,16 @@
+<?php
+	$school_name = $_POST['inpSchoolName'];
+	$school_type = $_POST['inpSchoolType'];
+	$school_addrlane1 = $_POST['inpaddrlane1'];
+	$school_addrlane2 = $_POST['inpaddrlane2'];
+	$school_city	= $_POST['inpcity'];
+	$school_state = $_POST['inpstate'];
+	$school_phone = $_POST['inpphone'];
+	$school_email = $_POST['inpEmail'];
+	$school_overview = $_POST['inpoverview'];
+
+	print $school_name;
+	print '<br>';
+	print $school_type;
+	print '<br>End of WS.';
+?>

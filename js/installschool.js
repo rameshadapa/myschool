8777ad14409b59f092be23f1c4e1bc1f84c00a15
Ramesh Adapa <ramesh.adapa86@gmail.com>
2015-07-28 17:49:50 +0530

    Updated installschool validation.

--- js/installschool.js
+++ js/installschool.js
@@ -13,6 +13,70 @@ $(document).ready(function() {
 		message: 'The school name is required and cannot be empty'
 	    }
 	  }
+	},
+	inpSchoolType: {
+	  validators: {
+	    notEmpty: {
+		message: 'The school type is required and cannot be empty'
+	    }
+	  }
+	},
+	inpaddrlane1: {
+	  validators: {
+	    notEmpty: {
+		message: 'The lane one address is required and cannot be empty'
+	    }
+	  }
+	},
+	inpaddrlane2: {
+	  validators: {
+	    notEmpty: {
+		message: 'The lane two address is required and cannot be empty'
+	    }
+	  }
+	},
+	inpcity: {
+	  validators: {
+	    notEmpty: {
+		message: 'The city is required and cannot be empty'
+	    }
+	  }
+	},
+	inpstate: {
+	  validators: {
+	    notEmpty: {
+		message: 'The state is required and cannot be empty'
+	    }
+	  }
+	},
+	inpphone: {
+	  validators: {
+	    notEmpty: {
+		message: 'The phone number is required and cannot be empty'
+	    },
+	    stringLength: {
+		min: 10,
+		max: 13,
+		message: 'The phone number must be below 10-13 characters length.'
+	    }
+	  }
+	},
+	inpEmail: {
+	  validators: {
+	    notEmpty: {
+		message: 'The email is required and cannot be empty'
+	    },
+	    emailAddress: {
+		message: 'The email address is not valid.'
+	    }
+	  }
+	},
+	inpoverview: {
+	  validators: {
+	    notEmpty: {
+		message: 'The overview is required and cannot be empty'
+	    }
+	  }
 	}
     }
   });
